f7ba780eba0c7e00073da8fd9d164d84c9309316
MalouOfTheDoom <mael.devpro@gmail.com>
2022-01-05 15:49:46 +0100

    added Customer and Transformation class small example with 3 customers

--- ProjectIMT/HomeView.swift
+++ ProjectIMT/HomeView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct HomeView: View {
+//    let john = Customer(first_name: "John")
+//    let marc = Customer(first_name: "Marc")
+//    let marie = Customer(first_name: "Marie")
+    var customers_list = [Customer(first_name: "John"),
+                          Customer(first_name: "Marc"),
+                          Customer(first_name: "Marie")]
+    
     var body: some View {
-        Text("Ici la liste des clients")
+        List(customers_list) {
+            Text($0.first_name)
+        }
     }
 }
 
