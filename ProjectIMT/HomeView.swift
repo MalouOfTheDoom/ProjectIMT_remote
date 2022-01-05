b36bf78b843a9e86b07b2b076190ba4fdf1408c9
MalouOfTheDoom <mael.devpro@gmail.com>
2022-01-05 14:38:02 +0100

    added MainView and tab Navigation

---
+++ ProjectIMT/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  ProjectIMT
+//
+//  Created by Maël Trouillet on 05/01/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Ici la liste des clients")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ ProjectIMT/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  ProjectIMT
+//
+//  Created by Maël Trouillet on 05/01/2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+            OptionsView()
+                .tabItem {
+                    Label("Options", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
