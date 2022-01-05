893bc8b0e064ad7f088fba95d7cf40d3984a472b
MalouOfTheDoom <mael.devpro@gmail.com>
2022-01-05 14:16:18 +0100

    Initial Commit

---
+++ ProjectIMT/ProjectIMTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectIMTApp.swift
+//  ProjectIMT
+//
+//  Created by Maël Trouillet on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjectIMTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
